bfcae8921d3d82fdd93e74c6779bd4bbcdd7ac25
opor-jitpanu <opor_tan@hotmail.co.th>
2018-11-11 23:36:28 +0700

    ticket

--- public/scripts/buy_ticket.js
+++ public/scripts/buy_ticket.js
@@ -1,12 +1,41 @@
     $(document).ready(function(){
-      $('.count').prop('disabled', true);
-      $(document).on('click','.plus',function(){
-        $('.count').val(parseInt($('.count').val()) + 1 );
+      $('.count1').prop('disabled', true);
+      $(document).on('click','.plus1',function(){
+        $('.count1').val(parseInt($('.count1').val()) + 1 );
       });
-      $(document).on('click','.minus',function(){
-        $('.count').val(parseInt($('.count').val()) - 1 );
-        if ($('.count').val() <= 0) {
-          $('.count').val(0);
+      $(document).on('click','.minus1',function(){
+        $('.count1').val(parseInt($('.count1').val()) - 1 );
+        if ($('.count1').val() <= 0) {
+          $('.count1').val(0);
+        }
+      });
+    });
+
+
+    $(document).ready(function(){
+      $('.count2').prop('disabled', true);
+      $(document).on('click','.plus2',function(){
+        $('.count2').val(parseInt($('.count2').val()) + 1 );
+      });
+      $(document).on('click','.minus2',function(){
+        $('.count2').val(parseInt($('.count2').val()) - 1 );
+        if ($('.count2').val() <= 0) {
+          $('.count2').val(0);
+        }
+      });
+    });
+
+
+
+    $(document).ready(function(){
+      $('.count3').prop('disabled', true);
+      $(document).on('click','.plus3',function(){
+        $('.count3').val(parseInt($('.count3').val()) + 1 );
+      });
+      $(document).on('click','.minus3',function(){
+        $('.count3').val(parseInt($('.count3').val()) - 1 );
+        if ($('.count3').val() <= 0) {
+          $('.count3').val(0);
         }
       });
     });
