a6766ebf0abf9e3d94b4ba3cb7edd9982f5d4bf7
opor-jitpanu <opor_tan@hotmail.co.th>
2018-11-14 14:23:39 +0700

    css

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -15,9 +15,11 @@ window.onload = function(){
           .then(function(snapshot){
             var firstname = snapshot.child(id).child("firstname").val();
             var lastname = snapshot.child(id).child("lastname").val();
+            var email = snapshot.child(id).child("email").val();
             var tel = snapshot.child(id).child("tel").val();
             document.getElementById("firstlastname").innerHTML = firstname + ' '+lastname;
             document.getElementById("tel").innerHTML = tel;
+            document.getElementById("email").innerHTML = email;
             document.getElementById("loading").style.display = "none";
           });
         });
