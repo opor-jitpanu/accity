553ec2dbfc660fe914c00c4a8bf54b1842f9e0fe
opor-jitpanu <opor_tan@hotmail.co.th>
2018-11-18 23:01:02 +0700

    my ticket

--- public/scripts/my_ticket.js
+++ public/scripts/my_ticket.js
@@ -1,12 +1,7 @@
+var countNormal = 0; 
+
+
 window.onload = function(){
-
-
-
-
-
-
-
-
 
 	firebase.auth().onAuthStateChanged(function(user) {
 		var email = user.email;
@@ -28,7 +23,7 @@ window.onload = function(){
 
 		today = dd + '/' + mm + '/' + yyyy;
 		userDataRef.orderByChild("email").equalTo(email).once("value").then(function(snapshot) {
-			var countNormal = 0; 
+			
 			var countAcNormal = 0; 
 			var barcode = '';
 			snapshot.forEach(function(childSnapshot) {
@@ -91,11 +86,18 @@ window.onload = function(){
 				$('#barcode2').hide();
 			}
 
+
+
+			if (countNormal == 0) {
+				$('#btn_modal').hide();
+			}
+
 		});
 	});
 
 
 
+}
 
 
 
@@ -106,37 +108,61 @@ window.onload = function(){
 
 
 
+function ticketactivateminusOnClick(){
+	var ticket = document.getElementById('ticket1').value;
+      // console.log(ticket);
+      if (ticket > 0) {
+      	ticket = parseInt(ticket);
+      	ticket -= 1;
+        // console.log("tet");
+
+        document.getElementById("ticket1").value = ticket;
+    }
+
+}
+
+
+function ticketactivateplusOnClick(){
+
+	var ticket = document.getElementById('ticket1').value;
+      // console.log(ticket);
+      if (ticket >= 0 && ticket < countNormal) {
+      	ticket = parseInt(ticket);
+      	ticket += 1;
+        // console.log("tet");
+
+        document.getElementById("ticket1").value = ticket;
+    }
+
+}
 
 
 
+function SubmitCheckOnClick(){
+	var ticket = document.getElementById('ticket1').value;
+	var userDataRef = firebase.database().ref("Ticket");
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1;
+	var yyyy = today.getFullYear();
 
+	if(dd<10) {
+		dd = '0'+dd
+	} 
 
-	document.getElementById("ticket_normal").style.display = "none";
-	document.getElementById("ticket_year").style.display = "none";
-	document.getElementById("ticket_activated").style.display = "none";
+	if(mm<10) {
+		mm = '0'+mm
+	} 
 
+	today = dd + '/' + mm + '/' + yyyy;
+
+	var check = 0;
 	firebase.auth().onAuthStateChanged(function(user) {
 		var email = user.email;
-
-
-		var userDataRef = firebase.database().ref("Ticket");
-		var today = new Date();
-		var dd = today.getDate();
-		var mm = today.getMonth()+1;
-		var yyyy = today.getFullYear();
-
-		if(dd<10) {
-			dd = '0'+dd
-		} 
-
-		if(mm<10) {
-			mm = '0'+mm
-		} 
-
-		today = dd + '/' + mm + '/' + yyyy;
 		userDataRef.orderByChild("email").equalTo(email).once("value").then(function(snapshot) {
-
 			snapshot.forEach(function(childSnapshot) {
+
+
 
 				var key = childSnapshot.key;
 				var childData = childSnapshot.val();
@@ -155,129 +181,68 @@ window.onload = function(){
 				var issue = childSnapshot.val().issue;
 				var valid = childSnapshot.val().valid;
 
+				
+
 				if (date_in == today || status == "stanby") {
-
-
 
 					if (status == "stanby" && type == "normal") {
 
 
-						document.getElementById("ticket_normal").style.display = "block";
-						var table = document.getElementById("myTable");
-						var row = table.insertRow(0);
+						if (check < ticket) {
+							console.log(key);
+							var today = new Date();
+							var dd = today.getDate();
+							var mm = today.getMonth()+1;
+							var yyyy = today.getFullYear();
 
-						console.log("CHECK");
-						
+							if(dd<10) {
+								dd = '0'+dd
+							} 
 
-						var cell_status = row.insertCell(0);
-						cell_status.innerHTML = '<center><a href="activate_ticket.html?ticket='+key+'" ><button class="btn btn-primary">Activate</button></a><center>';
+							if(mm<10) {
+								mm = '0'+mm
+							} 
 
-						var cell_time_in = row.insertCell(1);
-						cell_time_in.innerHTML = '<center><a href="transfer_ticket.html?ticket='+key+'" ><button class="btn btn-danger">Transfer</button></a><center>';
+							today = dd + '/' + mm + '/' + yyyy;
 
+							console.log(today);
 
+							var ref = firebase.database().ref("Ticket");
+							ref.child(key)
+							.update({ 
+								status : 'activated',
+								date_in : today
 
-
-
-
-					}else if (status == "stanby" && type == "year") {
-						document.getElementById("ticket_year").style.display = "block";
-						var table = document.getElementById("myTable2");
-						var row = table.insertRow(0);
-
-						console.log("CHECK");
-						
-
-						var cell_status = row.insertCell(0);
-						cell_status.innerHTML = '<center><a href="activate_ticket.html?ticket='+key+'" ><button class="btn btn-primary">Activate</button></a><center>';
-
-						var cell_time_in = row.insertCell(1);
-						cell_time_in.innerHTML = '<center><a href="transfer_ticket.html?ticket='+key+'" ><button class="btn btn-danger">Transfer</button></a><center>';
-
-
-					}
-					else if (status == "activated") {
-
-						document.getElementById("ticket_activated").style.display = "block";
-
-						var table3 = document.getElementById("myTable3");
-						var row3 = table3.insertRow(0);
-						var cell_status = row3.insertCell(0);
-						cell_status.innerHTML = "Activated";
-
-						var cell_time_in = row3.insertCell(1);
-						cell_time_in.innerHTML = issue;
-
-						var cell_date_in = row3.insertCell(2);
-						cell_date_in.innerHTML = valid;
-
-						var cell_date_in = row3.insertCell(3);
-						cell_date_in.innerHTML = type;
-
-						var cell_barcode = row3.insertCell(4);
-
-						cell_barcode.innerHTML = '<center><a href="show_barcode_ticket.html?ticket='+key+'" ><button class="btn btn-red">Barcode</button></a><center>';
-
+							});
+							check += 1;
+						}else if (check == ticket) {
+							window.location.href = "my_ticket.html";
+						}else{
+							window.location.href = "my_ticket.html";
+						}
 
 					}
 
 				}
 
 			});
-
-
-			lastShowTicket();
-
-			
-
-
+			if (check == ticket) {
+				window.location.href = "my_ticket.html";
+			}
 		});
+
+
+		
+		
 	});
 
-
-
-
-}
-
-
-
-function lastShowTicket(){
-	var table = document.getElementById("myTable3");
-	var header = table.createTHead();
-	var row = header.insertRow(0);
-	var cell = row.insertCell(0);
-	cell.innerHTML = "<center><b>Status</b></center>";
-	var cell = row.insertCell(1);
-	cell.innerHTML = "<center><b>issue</b></center>";
-	var cell = row.insertCell(2);
-	cell.innerHTML = "<center><b>valid</b></center>";
-	var cell = row.insertCell(3);
-	cell.innerHTML = "<center><b>type</b></center>";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function normalOnClick(){
-	document.getElementById("normal_ticket").style.display = "block";
-	document.getElementById("year_ticket").style.display = "none";
-}
-
-
-function yearOnClick(){
-	document.getElementById("year_ticket").style.display = "block";
-	document.getElementById("normal_ticket").style.display = "none";
 	
+	
+
+
+
+
+
 }
 
 
@@ -288,114 +253,3 @@ function yearOnClick(){
 
 
 
-
-
-
-
-
-(function() {
-	$(document).ready(function() {
-		var walkthrough;
-		walkthrough = {
-			index: 0,
-			nextScreen: function() {
-				if (this.index < this.indexMax()) {
-					this.index++;
-					return this.updateScreen();
-				}
-			},
-			prevScreen: function() {
-				if (this.index > 0) {
-					this.index--;
-					return this.updateScreen();
-				}
-			},
-			updateScreen: function() {
-				this.reset();
-				this.goTo(this.index);
-				return this.setBtns();
-			},
-			setBtns: function() {
-				var $lastBtn, $nextBtn, $prevBtn;
-				$nextBtn = $('.next-screen');
-				$prevBtn = $('.prev-screen');
-				$lastBtn = $('.finish');
-				if (walkthrough.index === walkthrough.indexMax()) {
-					$nextBtn.prop('disabled', true);
-					$prevBtn.prop('disabled', false);
-					return $lastBtn.addClass('active').prop('disabled', false);
-				} else if (walkthrough.index === 0) {
-					$nextBtn.prop('disabled', false);
-					$prevBtn.prop('disabled', true);
-					return $lastBtn.removeClass('active').prop('disabled', true);
-				} else {
-					$nextBtn.prop('disabled', false);
-					$prevBtn.prop('disabled', false);
-					return $lastBtn.removeClass('active').prop('disabled', true);
-				}
-			},
-			goTo: function(index) {
-				$('.screen').eq(index).addClass('active');
-				return $('.dot').eq(index).addClass('active');
-			},
-			reset: function() {
-				return $('.screen, .dot').removeClass('active');
-			},
-			indexMax: function() {
-				return $('.screen').length - 1;
-			},
-			closeModal: function() {
-				$('.walkthrough, .shade').removeClass('reveal');
-				return setTimeout(((function(_this) {
-					return function() {
-						$('.walkthrough, .shade').removeClass('show');
-						_this.index = 0;
-						return _this.updateScreen();
-					};
-				})(this)), 200);
-			},
-			openModal: function() {
-				$('.walkthrough, .shade').addClass('show');
-				setTimeout(((function(_this) {
-					return function() {
-						return $('.walkthrough, .shade').addClass('reveal');
-					};
-				})(this)), 200);
-				return this.updateScreen();
-			}
-		};
-		$('.next-screen').click(function() {
-			return walkthrough.nextScreen();
-		});
-		$('.prev-screen').click(function() {
-			return walkthrough.prevScreen();
-		});
-		$('.close').click(function() {
-			return walkthrough.closeModal();
-		});
-		$('.open-walkthrough').click(function() {
-			return walkthrough.openModal();
-		});
-		walkthrough.openModal();
-		return $(document).keydown(function(e) {
-			switch (e.which) {
-				case 37:
-				walkthrough.prevScreen();
-				break;
-				case 38:
-				walkthrough.openModal();
-				break;
-				case 39:
-				walkthrough.nextScreen();
-				break;
-				case 40:
-				walkthrough.closeModal();
-				break;
-				default:
-				return;
-			}
-			e.preventDefault();
-		});
-	});
-
-}).call(this);
--- public/staff/scripts/staff_check_time_in.js
+++ public/staff/scripts/staff_check_time_in.js
@@ -9,241 +9,284 @@ function BackHome(){
 
 function CheckTimeInOnClick(){
 	var ticket_id = document.getElementById('ticket_id').value;
-
-
+	var ticket = '';
 	
-
-
-	// var ref = firebase.database().ref("Ticket");
-	// ref.once("value")
-	// .then(function(snapshot){
-	// 	var time_in = snapshot.child(ticket_id).child("time_in").val();
-
-	// 	var type = snapshot.child(ticket_id).child("type").val();
-
-
+	for (i = 0; i < ticket_id.length; i++) {
+		// console.log(ticket_id[i]);
 		
-	// 	if (time_in == "-") {
-	// 		var d = new Date(); 
-	// 		var hour = d.getHours(); 
-	// 		var minute = d.getMinutes();
 
-	// 		if (minute < 10) {
-	// 			minute = '0' + minute;
-	// 		}
+		if (ticket_id[i] != '*') {
 
-	// 		var time = hour + ":" + minute;
 
-	// 		var today = new Date();
-	// 		var dd = today.getDate();
-	// 		var mm = today.getMonth()+1;
-	// 		var yyyy = today.getFullYear();
 
+			console.log(ticket_id[i]);
+			ticket += ticket_id[i];
 
-	// 		var nextDay = new Date(today);
-	// 		nextDay.setDate(today.getDate()+1);
-	// 		var ndd = nextDay.getDate();
-	// 		var nmm = nextDay.getMonth()+1;
-	// 		var nyyyy = nextDay.getFullYear();
 
+		}else if (ticket_id[i] == '*' && ticket_id[i+1] == '*') {
 
-	// 		if(ndd<10) {
-	// 			ndd = '0'+ndd
-	// 		} 
 
-	// 		if(nmm<10) {
-	// 			nmm = '0'+nmm
-	// 		} 
 
 
-	// 		if(dd<10) {
-	// 			dd = '0'+dd
-	// 		} 
+			console.log(ticket);
 
-	// 		if(mm<10) {
-	// 			mm = '0'+mm
-	// 		} 
 
-	// 		today = dd + '/' + mm + '/' + yyyy;
-	// 		var valid = ndd + '/' + nmm + '/' + nyyyy;
 
 
-	// 		if (type == "normal") {
-	// 			var ref = firebase.database().ref("Ticket");
-	// 			ref.child(ticket_id)
-	// 			.update({ 
-	// 				time_in: time,
-	// 				issue:today,
-	// 				valid:valid
-	// 			});
-	// 		}else if (type == "year") {
 
-	// 			console.log('yeaar');
-	// 			var today = new Date();
+			var ref = firebase.database().ref("Ticket");
+			ref.once("value")
+			.then(function(snapshot){
+				console.log("TICKET : "+ticket);
+				var time_in = snapshot.child(ticket).child("time_in").val();
 
-	// 			var yyyy = today.getFullYear();
-	// 			var issue = "01/01/"+yyyy;
-	// 			var valid = "31/12/"+yyyy;
+				var type = snapshot.child(ticket).child("type").val();
 
 
-	// 			var ref = firebase.database().ref("Ticket");
-	// 			ref.child(ticket_id)
-	// 			.update({ 
-	// 				time_in: time,
-	// 				issue:issue,
-	// 				valid:valid
-	// 			});
-	// 		}
 
+				if (time_in == "-") {
+					var d = new Date(); 
+					var hour = d.getHours(); 
+					var minute = d.getMinutes();
 
-			
+					if (minute < 10) {
+						minute = '0' + minute;
+					}
 
-			
+					var time = hour + ":" + minute;
 
+					var today = new Date();
+					var dd = today.getDate();
+					var mm = today.getMonth()+1;
+					var yyyy = today.getFullYear();
 
 
+					var nextDay = new Date(today);
+					nextDay.setDate(today.getDate()+1);
+					var ndd = nextDay.getDate();
+					var nmm = nextDay.getMonth()+1;
+					var nyyyy = nextDay.getFullYear();
 
 
-	// 		var ref = firebase.database().ref("Ticket");
-	// 		ref.once("value")
-	// 		.then(function(snapshot){
-	// 			var email = snapshot.child(ticket_id).child("email").val();
-	// 			insertEmail(email);
-	// 		});
+					if(ndd<10) {
+						ndd = '0'+ndd
+					} 
 
+					if(nmm<10) {
+						nmm = '0'+nmm
+					} 
 
-	// 		function insertEmail(email){
 
-	// 			var ref = firebase.database().ref("Time");
-	// 			ref.orderByChild('email').equalTo(email).on("value", function(snapshot) {
+					if(dd<10) {
+						dd = '0'+dd
+					} 
 
-	// 				snapshot.forEach(function(data) {
+					if(mm<10) {
+						mm = '0'+mm
+					} 
 
-	// 					edit(data.key);
-	// 				});
-	// 			});
-	// 		}
+					today = dd + '/' + mm + '/' + yyyy;
+					var valid = ndd + '/' + nmm + '/' + nyyyy;
 
-	// 		function edit(id){
-	// 			var today_in = new Date();
-	// 			var dd_in = today_in.getDate();
-	// 			var mm_in = today_in.getMonth()+1;
-	// 			var yyyy_in = today_in.getFullYear();
 
-	// 			if(dd_in<10) {
-	// 				dd_in = '0'+dd_in;
-	// 			} 
+					if (type == "normal") {
+						var ref = firebase.database().ref("Ticket");
+						ref.child(ticket)
+						.update({ 
+							time_in: time,
+							issue:today,
+							valid:valid
+						});
+					}else if (type == "year") {
 
-	// 			if(mm_in<10) {
-	// 				mm_in = '0'+mm_in;
-	// 			} 
+						console.log('yeaar');
+						var today = new Date();
 
-	// 			date_in = dd_in + '/' + mm_in + '/' + yyyy_in;
+						var yyyy = today.getFullYear();
+						var issue = "01/01/"+yyyy;
+						var valid = "31/12/"+yyyy;
 
-	// 			var d_in = new Date(); 
-	// 			var hour_in = d_in.getHours();
-	// 			var minute_in = d_in.getMinutes(); 
 
-	// 			if (minute_in < 10) {
-	// 				minute_in = '0' + minute_in;
-	// 			}
+						var ref = firebase.database().ref("Ticket");
+						ref.child(ticket)
+						.update({ 
+							time_in: time,
+							issue:issue,
+							valid:valid
+						});
+					}
 
-	// 			var time_in = hour_in + ":" + minute_in;
 
-	// 			var ref = firebase.database().ref("Time");
-	// 			ref.child(id)
-	// 			.update({ date: date_in,
-	// 				time_in: time_in,
-	// 				time_out: '-'
-	// 			});
 
 
-	// 		}
-	// 		alert("Time in Complete!");
-	// 	}else if (type == "year") {
-	// 		var valid = snapshot.child(ticket_id).child("valid").val();
-	// 		var issue = snapshot.child(ticket_id).child("issue").val();
-	// 		console.log(valid);
-	// 		console.log(issue);
 
-	// 		var today = new Date();
-	// 		var dd = today.getDate();
-	// 		var mm = today.getMonth()+1;
-	// 		var yyyy = today.getFullYear();
 
-	// 		today = dd + '/' + mm + '/' + yyyy;
 
-	// 		var dateFrom = issue;
-	// 		var dateTo = valid;
-	// 		var dateCheck = today;
 
-	// 		var d1 = dateFrom.split("/");
-	// 		var d2 = dateTo.split("/");
-	// 		var c = dateCheck.split("/");
 
-	// 		var from = new Date(d1[2], parseInt(d1[1])-1, d1[0]);
-	// 		var to   = new Date(d2[2], parseInt(d2[1])-1, d2[0]);
-	// 		var check = new Date(c[2], parseInt(c[1])-1, c[0]);
 
-	// 		console.log(check > from && check < to)
+					var ref = firebase.database().ref("Ticket");
+					ref.once("value")
+					.then(function(snapshot){
+						var email = snapshot.child(ticket).child("email").val();
+						insertEmail(email);
+					});
 
-	// 		console.log(dateFrom);
-	// 		console.log(dateTo);
-	// 		console.log(dateCheck);
 
-	// 		if (check > from && check < to) {
-	// 			var firebaseRef3 = firebase.database().ref("History");
-	// 			firebaseRef3.push({
-	// 				id:ticket_id,
-	// 				date:today
-	// 			});
+					function insertEmail(email){
 
+						var ref = firebase.database().ref("Time");
+						ref.orderByChild('email').equalTo(email).on("value", function(snapshot) {
 
+							snapshot.forEach(function(data) {
 
-	// 			var ref = firebase.database().ref("Ticket");
-	// 			ref.once("value")
-	// 			.then(function(snapshot){
-	// 				var email = snapshot.child(ticket_id).child("email").val();
-	// 				var ref = firebase.database().ref("Time");
-	// 				ref.orderByChild('email').equalTo(email).on("value", function(snapshot) {
+								edit(data.key);
+							});
+						});
+					}
 
-	// 					snapshot.forEach(function(data) {
+					function edit(id){
+						var today_in = new Date();
+						var dd_in = today_in.getDate();
+						var mm_in = today_in.getMonth()+1;
+						var yyyy_in = today_in.getFullYear();
 
-	// 						var ref = firebase.database().ref("Time");
-	// 						ref.child(data.key)
-	// 						.update({ date: today
-	// 						});
-	// 						console.log("check");
-	// 					});
-	// 				});
+						if(dd_in<10) {
+							dd_in = '0'+dd_in;
+						} 
 
-	// 			});
+						if(mm_in<10) {
+							mm_in = '0'+mm_in;
+						} 
 
+						date_in = dd_in + '/' + mm_in + '/' + yyyy_in;
 
+						var d_in = new Date(); 
+						var hour_in = d_in.getHours();
+						var minute_in = d_in.getMinutes(); 
 
-				
+						if (minute_in < 10) {
+							minute_in = '0' + minute_in;
+						}
 
+						var time_in = hour_in + ":" + minute_in;
 
+						var ref = firebase.database().ref("Time");
+						ref.child(id)
+						.update({ date: date_in,
+							time_in: time_in,
+							time_out: '-'
+						});
 
 
+					}
+					alert("Time in Complete!");
+				}else if (type == "year") {
+					var valid = snapshot.child(ticket).child("valid").val();
+					var issue = snapshot.child(ticket).child("issue").val();
+					console.log(valid);
+					console.log(issue);
 
+					var today = new Date();
+					var dd = today.getDate();
+					var mm = today.getMonth()+1;
+					var yyyy = today.getFullYear();
 
+					today = dd + '/' + mm + '/' + yyyy;
 
+					var dateFrom = issue;
+					var dateTo = valid;
+					var dateCheck = today;
 
-				
-	// 		}
+					var d1 = dateFrom.split("/");
+					var d2 = dateTo.split("/");
+					var c = dateCheck.split("/");
 
-	// 		alert("Ticket Year can use");
+					var from = new Date(d1[2], parseInt(d1[1])-1, d1[0]);
+					var to   = new Date(d2[2], parseInt(d2[1])-1, d2[0]);
+					var check = new Date(c[2], parseInt(c[1])-1, c[0]);
 
+					console.log(check > from && check < to)
 
+					console.log(dateFrom);
+					console.log(dateTo);
+					console.log(dateCheck);
 
-	// 	}
+					if (check > from && check < to) {
+						var firebaseRef3 = firebase.database().ref("History");
+						firebaseRef3.push({
+							id:ticket,
+							date:today
+						});
 
 
-	// 	else{
-	// 		alert("Sorry, Ticket is Activated");
-	// 	}
-	// });
+
+						var ref = firebase.database().ref("Ticket");
+						ref.once("value")
+						.then(function(snapshot){
+							var email = snapshot.child(ticket).child("email").val();
+							var ref = firebase.database().ref("Time");
+							ref.orderByChild('email').equalTo(email).on("value", function(snapshot) {
+
+								snapshot.forEach(function(data) {
+
+									var ref = firebase.database().ref("Time");
+									ref.child(data.key)
+									.update({ date: today
+									});
+									console.log("check");
+								});
+							});
+
+						});
+
+
+
+
+
+
+
+
+
+
+
+
+
+					}
+
+					alert("Ticket Year can use");
+
+
+
+				}
+
+
+				else{
+					alert("Sorry, Ticket is Activated");
+				}
+			});
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(ticket);
+console.log("CHeck");
+var ticket = '';
+
+}
+}
+
+
+
 
 
 
@@ -254,3 +297,27 @@ function CheckTimeInOnClick(){
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
