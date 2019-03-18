3dd94126493b7e7cf9b2064d6de8f146f8d365ec
opor-jitpanu <opor_tan@hotmail.co.th>
2019-03-18 12:38:43 +0700

    google login templater

---
+++ public/mobi/js/login_google.js
@@ -0,0 +1,24 @@
+function signInOnClick(){
+
+
+	
+
+
+
+	document.getElementById("loading").style.display = 'block';
+	
+	var email = document.getElementById('email_input').value;
+	var password = document.getElementById('password_input').value;
+
+	
+
+
+
+	
+}
+
+
+
+window.onload = function(){
+	document.getElementById("loading").style.display = 'none';
+}
